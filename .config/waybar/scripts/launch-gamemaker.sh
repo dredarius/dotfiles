747e4384f58bc480fb9379adfd6ae11a0c767ade
Josh Gillespie <joshgillespie@gmail.com>
2026-04-10 22:21:22 -0400

    Update waybar config and style

---
+++ .config/waybar/scripts/launch-gamemaker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+WINEPREFIX=~/.local/share/Steam/steamapps/compatdata/1808500/pfx \
+  wine ~/.local/share/Steam/steamapps/compatdata/1808500/pfx/drive_c/Program\ Files/GameMaker/GameMaker.exe &
